5710518d500233744a6b8d24702edc561046d98c
J SHAKTHI SRAVANTH <shakthisravanth001@gmail.com>
2026-02-05 13:27:03 +0530

    lab - 7: Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleSubmit(event) {
+		event.preventDefault();
+
+		let studentName = document.getElementById("name").value;
+		let studentAge = document.getElementById("age").value;
+
+		document.getElementById("output").innerText =
+				"Name: " + studentName + ", Age: " + studentAge;
+}
