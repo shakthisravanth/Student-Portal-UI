4651c1afbe3ba646fe290d1f58ff0f009564706e
J SHAKTHI SRAVANTH <121029982+shakthisravanth@users.noreply.github.com>
2026-02-06 16:59:17 +0530

    Update and rename StudentApi.java to StudentAPI.java

--- StudentAPI/src/main/java/com/StudentAPI.java
+++ StudentAPI/src/main/java/com/StudentAPI.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentApi{
+public class StudentAPI{
 
     public static void main(String[] args) {
         SpringApplication.run(StudentApi.class, args);
