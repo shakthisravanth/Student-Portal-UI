626d17caa641a63b7c4a1a7ad700349c68a0e10e
J SHAKTHI SRAVANTH <shakthisravanth001@gmail.com>
2026-02-06 16:30:22 +0530

    lab - 9: Initial commit

--- Frontend/script.js
+++
@@ -1,8 +0,0 @@
-function handleSubmit(event) {
-	event.preventDefault();
-
-	let studentName = document.getElementById("name").value;
-	let studentAge = document.getElementById("age").value;
-
-	document.getElementById("output").innerText = "Name: " + studentName + ", Age: " + studentAge;
-}
